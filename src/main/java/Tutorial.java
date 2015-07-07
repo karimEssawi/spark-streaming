bb68598ade2fc0a308bf6f7e12329073d0d2d272
Karim Essawi <karim.essawi@autotrader.co.uk>
2015-07-07 16:31:51 +0100

    Updated gradle to 2.4

--- src/main/java/Tutorial.java
+++ src/main/java/Tutorial.java
@@ -1,10 +1,14 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.twitter.TwitterUtils;
+import scala.Tuple2;
 import twitter4j.Status;
+
+import java.util.Arrays;
 
 public class Tutorial {
 
@@ -13,10 +17,18 @@ public class Tutorial {
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaStreamingContext jssc = new JavaStreamingContext(sc, new Duration(2000));
 
-        String[] filters = new String[] {"England"};
+        String[] filters = new String[] {"egypt"};
 
-        JavaReceiverInputDStream<Status> stream = TwitterUtils.createStream(jssc);
-        stream.print();
+        JavaDStream<Status> stream = TwitterUtils.createStream(jssc, filters);
+        JavaDStream<String> hashTags = stream.flatMap(t -> Arrays.asList(t.getText().split(" "))).filter(h -> h.startsWith("#"));
+
+        // Count the hashtags over a 5 minute window
+        // Map each tag to a (tag, 1) key-value pair
+        JavaPairDStream<String, Integer> tuples = hashTags.mapToPair(h -> new Tuple2(h, 1));
+        // Then reduce by adding the counts
+        JavaPairDStream<String, Integer> counts = tuples.reduceByKeyAndWindow((a, b) -> a + b, (a, b) -> a - b, new Duration(60 * 5 * 1000), new Duration(1 * 1000));
+
+        counts.print();
 
         jssc.start();
         jssc.awaitTermination();
